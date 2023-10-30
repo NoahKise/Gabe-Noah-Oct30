f234aa9a42c63e4a9e3fc979b241a90cca3dab05
noahkise <noah@kisefamily.com>
2023-10-30 11:34:54 -0700

    attempt return name as button
    Co-authored-by: Gabriel Tucker <gabrieltucker260@gmail.com>

--- places-youve-been/js/scripts.js
+++ places-youve-been/js/scripts.js
@@ -11,6 +11,8 @@ function Place(landmark, location, date, notes) {
 let placesArray = [];
 const body = document.querySelector("body");
 
+
+
 function formHandler(event) {
     event.preventDefault();
     const landmarkInput = document.querySelector("input#inputLandmark").value;
@@ -24,16 +26,17 @@ function formHandler(event) {
     placesArray.push(newPlace);
     console.log(newPlace);
     console.log(placesArray);
-    // const nameDiv = document.createElement("div")
-    // nameDiv.setAttribute("")
-    const resultDiv = document.createElement("div")
-    resultDiv.setAttribute("class", "result")
+    const nameDiv = document.createElement("div");
+    nameDiv.setAttribute("id", "resultName");
+    const resultDiv = document.createElement("div");
+    resultDiv.setAttribute("class", "result");
+    resultDiv.setAttribute("id", "hidden");
     const h1Landmark = document.createElement("h1");
     const h2Location = document.createElement("h2");
     const h3Date = document.createElement("h3");
     const pNotes = document.createElement("p");
     const imgImage = document.createElement("img");
-    
+
     h1Landmark.append(landmarkInput);
     h2Location.append(locationInput);
     h3Date.append(dateInput);
@@ -42,14 +45,18 @@ function formHandler(event) {
     if (imageInput) {
         const reader = new FileReader();
         reader.onload = function (e) {
-            imgImage.src = e.target.result; // Set the image source when it's loaded
+            imgImage.src = e.target.result;
         };
-        reader.readAsDataURL(imageInput); // Read the image as a data URL
+        reader.readAsDataURL(imageInput);
     }
-
-    resultDiv.append(h1Landmark, imgImage, h2Location, h3Date, pNotes)
+    nameDiv.append(h1Landmark)
+    resultDiv.append(imgImage, h2Location, h3Date, pNotes)
     resultDiv.style.backgroundColor = colorInput;
-    document.body.append(resultDiv);
+    document.body.append(nameDiv, resultDiv);
+    function reveal() {
+        resultDiv.removeAttribute("id")
+    }
+    // nameDiv.onclick = reveal();
 }
 
 window.addEventListener("load", function () {
